9d17162bedac61f80dc1f923b383473f787fa737
Alec Pizziferro <alecpizz@gmail.com>
2024-04-04 13:45:19 -0500

    sql lite installed

--- server/server.js
+++ server/server.js
@@ -1,14 +1,22 @@
-import express from "express";
-import cors from "cors";
+const express = require('express');
+const cors = require('cors');
 // import records from "./routes/record.js";
 
-
+const fs = require('fs');
+const initSqlJs = require('sql.js');
+const filebuffer = fs.readFileSync('test.sqlite');
 
 const PORT = process.env.PORT || 5050;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+
+initSqlJs().then(function (SQL) {
+  // Load the db
+  const db = new SQL.Database(filebuffer);
+});
 // app.use("/record", records);
 
 // start the Express server
