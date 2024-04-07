233aaa7a221af53bda3fddadf9dfee745e9114d2
Alec Pizziferro <alecpizz@gmail.com>
2024-04-06 23:23:16 -0500

    sqlite3, setup prompting in terminal for server

--- server/server.js
+++ server/server.js
@@ -1,25 +1,59 @@
-const express = require('express');
-const cors = require('cors');
-// import records from "./routes/record.js";
-
-const fs = require('fs');
-const initSqlJs = require('sql.js');
-const filebuffer = fs.readFileSync('test.sqlite');
+import express from 'express'
+import cors from 'cors'
+import sqlite3 from 'sqlite3'
+const db = new sqlite3.Database('test.sqlite');
+import rl from 'readline'
+const readline = rl.createInterface({ input: process.stdin, output: process.stdout });
 
 const PORT = process.env.PORT || 5050;
 const app = express();
 
+
 app.use(cors());
 app.use(express.json());
 
-
-initSqlJs().then(function (SQL) {
-  // Load the db
-  const db = new SQL.Database(filebuffer);
+app.listen(PORT, async () => {
+  console.log(`\nServer listening on port ${PORT}`);
+  db.each("SELECT * from LOREM", (err, row) => {
+    console.log(row);
+  });
 });
-// app.use("/record", records);
 
-// start the Express server
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
-});
+function askQuestion() {
+  readline.question("Enter a query or 'exit' to quit: ", (answer) => {
+    if (answer.toLowerCase() === 'exit') {
+      console.log('exiting!');
+      db.close();
+      readline.close();
+      process.exit();
+    }
+    console.log(`ASKING DB: "${answer}"`);
+    db.exec(answer);
+    askQuestion();
+  })
+}
+
+askQuestion();
+
+// while(!exit)
+// {
+//     const readline = rl.createInterface({input: process.stdin, output: process.stdout});
+//     readline.question("Enter an SQL query or 'exit' to exit: ", (answer) => {
+//       readline.close();
+//       if(answer == 'exit')
+//       {
+//         exit = true;
+//       }
+//       else
+//       {
+//           db.run(answer);
+//       }
+//     });
+// }
+
+// process.on("SIGINT", function () {
+//   //graceful shutdown
+//   console.log('exiting!');
+//   db.close();
+//   process.exit();
+// });
